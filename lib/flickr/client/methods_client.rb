34ae33ffd2a38755fd30c05dd1712588b24b5a03
Janko Marohnić <janko.marohnic@gmail.com>
2012-10-28 02:31:41 +0200

    Remove a leftover

--- lib/flickr/client/methods_client.rb
+++ lib/flickr/client/methods_client.rb
@@ -18,21 +18,5 @@ class Flickr
     def parser
       FaradayMiddleware::ParseJson
     end
-
-    private
-
-    def fix_extras(params)
-      if params.delete(:include_sizes)
-        urls = Flickr::Media::SIZES.values.map {|abbr| "url_#{abbr}" }.join(",")
-        include_in_extras(params, urls)
-      end
-      if params.delete(:include_media)
-        include_in_extras(params, "media")
-      end
-    end
-
-    def include_in_extras(params, things)
-      params[:extras] = [params[:extras], things].compact.join(",")
-    end
   end
 end
